c7de7f236532e7f994d205362f164e6838b0126f
Roni E. Frias <ronifrias17@gmail.com>
2024-05-21 17:52:27 -0400

    Validacion de formularios

--- js/validar-cuil.js
+++ js/validar-cuil.js
@@ -1,12 +1,19 @@
 export default function esUnCuil(campo) {
     const cuil = campo.value.replace(/[-\/]/g, "");
-    tieneNumerosRepetidos(cuil);
 
-    //console.log(cuil);
-    //console.log(tieneNumerosRepetidos(cuil));
-    console.log(validarPrimerosDigitos(cuil));
-    validarDigitoVerificador(cuil)
-    console.log(validarDigitoVerificador(cuil));
+    if (tieneNumerosRepetidos(cuil)) {
+        console.log("Valores repetidos");
+        campo.setCustomValidity("Vlaores repetidos")
+    } else {
+        if (validarPrimerosDigitos(cuil) && validarDigitoVerificador(cuil)) {
+            console.log("Cuil válido");
+
+        } else {
+            console.log("Cuil no existe");
+            campo.setCustomValidity("Cuil no existe");
+
+        }
+    }
 }
 
 function tieneNumerosRepetidos(cuil) {
